ed04a18b6ae7cfd8725d791cea90c5b088b26fb1
Abdulsalam Adams <codewithaplus@gmail.com>
2024-09-16 01:20:21 -0930

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors, { CorsOptions } from "cors";
-import swagger from "./docs/swagger";
+// import swagger from "./docs/swagger";
 
 import customerRoutes from "./Modules/Customer/customer.routes";
 // import { config } from './config/config';
@@ -22,11 +22,11 @@ app.use(bodyParser.json());
 app.use(cors(corsOptions));
 app.use(cookieParser());
 
-app.use(
-  "/api-docs",
-  swagger.getSwaggerUi().serve,
-  swagger.getSwaggerUi().setup(swagger.getSpecs())
-);
+// app.use(
+//   "/api-docs",
+//   swagger.getSwaggerUi().serve,
+//   swagger.getSwaggerUi().setup(swagger.getSpecs())
+// );
 app.use("/api/v2/customers", customerRoutes);
 
 export default app;
