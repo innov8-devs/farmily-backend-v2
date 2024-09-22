6198d7442c42b2ece03d9db8098b05a94153cb44
0xaplus <codewithaplus@gmail.com>
2024-09-22 16:47:17 +0100

    feat: validate image

---
+++ src/Shared/Middlewares/validateImages.middleware.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from "express";
+import { ImageServices } from "../../Modules/Image/image.services";
+
+export async function validateImages(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  
+  const files = req.files as
+    | Express.Multer.File[]
+    | { [fieldname: string]: Express.Multer.File[] };
+
+  if (!files || Object.keys(files).length === 0) {
+    return res.status(400).json({ message: "No images uploaded" });
+  }
+
+  // Handle the case where files are an array or an object with field names
+  const fileArray: Express.Multer.File[] = Array.isArray(files)
+    ? files
+    : Object.values(files).flat();
+
+  const allowedImageTypes = ["image/jpeg", "image/png", "image/jpg"];
+  for (const file of fileArray) {
+    if (!allowedImageTypes.includes(file.mimetype)) {
+      await ImageServices.deleteImageFromDisk(file.path);
+      return res.status(400).json({
+        message: "Invalid image file type. Allowed types: JPEG, PNG, JPG",
+      });
+    }
+
+    // Check the file size for each file (you can adjust the maximum size as needed)
+    const maxFileSize = 5 * 1024 * 1024; // 5 MB
+    if (file.size > maxFileSize) {
+      await ImageServices.deleteImageFromDisk(file.path);
+      return res.status(400).json({
+        error: "Image file size exceeds the maximum allowed size (5MB)",
+      });
+    }
+  }
+
+  // If all checks pass, proceed to the next middleware
+  next();
+}
