0a04c84a0b2f908ec8e31c428734b848067154ab
0xaplus <codewithaplus@gmail.com>
2024-09-27 23:00:21 +0100

    remove populate accounTypeId

--- src/Modules/Account/account.repository.ts
+++ src/Modules/Account/account.repository.ts
@@ -1,15 +1,15 @@
 import accountModel from './account.model';
 import { BaseRepository } from '../../Repository';
-import { FilterQuery } from 'mongoose';
+// import { FilterQuery } from 'mongoose';
 
 class AccountRepository extends BaseRepository<any> {
   constructor() {
     super(accountModel);
   }
 
-  async findOne(filter: FilterQuery<any>): Promise<any> {
-    return await this.model.findOne(filter).populate('accountTypeId').exec();
-  }
+  // async findOne(filter: FilterQuery<any>): Promise<any> {
+  //   return await this.model.findOne(filter).populate('accountTypeId').exec();
+  // }
 }
 
 export default new AccountRepository();
