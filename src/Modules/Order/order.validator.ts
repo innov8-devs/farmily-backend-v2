5b5fdccc20cad4ea8f1358881f50ced6f9af018a
0xaplus <codewithaplus@gmail.com>
2024-10-23 18:28:22 +0100

    fix typo

--- src/Modules/Order/order.validator.ts
+++ src/Modules/Order/order.validator.ts
@@ -23,7 +23,7 @@ export const orderValidator = (data) => {
 
     delivery: Joi.object({
       deliverAt: Joi.date().iso().optional(),
-      instrution: Joi.string().optional(),
+      instruction: Joi.string().optional(),
       mode: Joi.string().valid("To_Door", "Pick_Up").required(),
     }).required(),
 
